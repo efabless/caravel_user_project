f09a4cd88bda55e1aab743ffe120114e364a755b
manarabdelaty <manarabdelatty@aucegypt.edu>
2021-04-19 20:05:54 +0200

    [CI] update precheck scripts to pass CARAVEL_ROOT

--- .github/scripts/precheck/run-precheck.sh
+++ .github/scripts/precheck/run-precheck.sh
@@ -14,11 +14,12 @@
 # limitations under the License.
 # SPDX-License-Identifier: Apache-2.0
 export TARGET_PATH=$(pwd)
+export CARAVEL_ROOT=$(pwd)/caravel-lite
 cd ..
 export PDK_ROOT=$(pwd)/precheck_pdks
 cd $TARGET_PATH/open_mpw_precheck/
 
-docker run -v $(pwd):/usr/local/bin -v $TARGET_PATH:$TARGET_PATH -v $PDK_ROOT:$PDK_ROOT -u $(id -u $USER):$(id -g $USER) efabless/open_mpw_precheck:latest bash -c "python3 open_mpw_prechecker.py --skip_drc -p $PDK_ROOT -t $TARGET_PATH -ms develop"
+docker run -v $(pwd):/usr/local/bin -v $TARGET_PATH:$TARGET_PATH -v $PDK_ROOT:$PDK_ROOT -v $CARAVEL_ROOT:$CARAVEL_ROOT -u $(id -u $USER):$(id -g $USER) efabless/open_mpw_precheck:latest bash -c "python3 open_mpw_prechecker.py --skip_drc -p $PDK_ROOT -t $TARGET_PATH -c $CARAVEL_ROOT -ms develop"
 output=$TARGET_PATH/checks/full_log.log
 
 gzipped_file=$TARGET_PATH/checks/full_log.log.gz
