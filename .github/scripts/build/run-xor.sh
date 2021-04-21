16848c02da1d7f4329d2b7b5ae2570ca54aa2a5d
manarabdelaty <manarabdelatty@aucegypt.edu>
2021-04-21 15:34:39 +0200

    [CI] Fail if xor didn't pass

--- .github/scripts/build/run-xor.sh
+++ .github/scripts/build/run-xor.sh
@@ -25,4 +25,12 @@ cd $UPRJ_ROOT
 LOG_FILE=out.log
 docker run -v $UPRJ_ROOT:$UPRJ_ROOT -v $PDK_ROOT:$PDK_ROOT -v $CARAVEL_ROOT:$CARAVEL_ROOT -e UPRJ_ROOT=$UPRJ_ROOT -e PDK_ROOT=$PDK_ROOT -e CARAVEL_ROOT=$CARAVEL_ROOT -u $(id -u $USER):$(id -g $USER) $IMAGE_NAME bash -c "cd $UPRJ_ROOT; export USER_ID=$USER_ID; make xor-wrapper | tee $LOG_FILE;"
 
+cnt=$(grep -oP '(?<=Total XOR differences = )[0-9]+' $LOG_FILE)
+
+echo "Total XOR differences = $cnt"
+
+if [[ $cnt -ne 0 ]]; then 
+    exit 2; 
+fi
+
 exit 0
