589000b0c3b62553b20104f43d7c8e710392c5e1
AmoghLonkar <alonkar@ucsc.edu>
2021-05-02 21:17:49 -0700

    Instantiated storage module, set correct fields based on input packet

--- verilog/rtl/testchip/control_logic.v
+++ verilog/rtl/testchip/control_logic.v
@@ -16,3 +16,24 @@ module control_logic (
     output [7:0] read_addr,
     input [31:0] read_data
 );
+
+    //Instantiating storage module instance and
+    //connecting ports
+    storage connections(
+        // MGMT_AREA R/W Interface 
+        .mgmt_clk(clk_in),
+        .mgmt_ena(chip_select), 
+        .mgmt_wen(write_en), // not shared 
+        .mgmt_wen_mask(write_mask), // not shared 
+        .mgmt_addr(addr),
+        .mgmt_wdata(data),
+        .mgmt_rdata(???),
+
+        // MGMT_AREA RO Interface
+        .mgmt_ena_ro(read_enable),
+        .mgmt_addr_ro(read_addr),
+        .mgmt_rdata_ro(read_data)
+    );
+
+endmodule
+`default_nettype wire 
