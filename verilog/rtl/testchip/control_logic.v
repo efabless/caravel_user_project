3e2fb43f8d635741deb9e194401e89ba64645e17
AmoghLonkar <alonkar@ucsc.edu>
2021-05-03 23:19:53 -0700

    Split input packet into fields for storage module, ask how to select port

--- verilog/rtl/testchip/control_logic.v
+++ verilog/rtl/testchip/control_logic.v
@@ -26,6 +26,17 @@ module control_logic (
         in_packet <= packet
     end
 
+    assign chip_select = in_packet[(`RAM_BLOCKS*6+48):50 + 5*`RAM_BLOCKS-1];
+    assign write_en = in_packet[50 + 5*`RAM_BLOCKS-2:50+(`RAM_BLOCKS*4-1)]; 
+    assign write_mask = in_packet[49+(`RAM_BLOCKS*4-1):49];
+    assign addr = in_packet[48:41];
+    assign data = in_packet[40:9];
+    wire [31:0] rw_read_data;
+
+    assign read_enable = in_packet[8];
+    assign read_addr = in_packet[7:0];
+    wire [31:0] r_data;
+
     //Instantiating storage module instance and
     //connecting ports
     storage connections(
@@ -36,13 +47,20 @@ module control_logic (
         .mgmt_wen_mask(write_mask), // not shared 
         .mgmt_addr(addr),
         .mgmt_wdata(data),
-        .mgmt_rdata(???),
+        .mgmt_rdata(rw_read_data),
 
         // MGMT_AREA RO Interface
         .mgmt_ena_ro(read_enable),
         .mgmt_addr_ro(read_addr),
-        .mgmt_rdata_ro(read_data)
+        .mgmt_rdata_ro(r_data)
     );
 
+    //Mux read output from correct port
+    always@(rw_read_data or r_data or ???) begin
+        if(???)
+        assign read_to_pico = rw_read_data
+        else
+        assign read_to_pico = r_data
+    end
 endmodule
 `default_nettype wire 
