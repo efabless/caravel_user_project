f9a9f3a3e0232ab12cc0b118665d434435d11535
AmoghLonkar <alonkar@ucsc.edu>
2021-05-08 22:15:15 -0700

    Instantiated two SRAM chips, need to connect wires to input via demux

--- verilog/rtl/testchip/control_logic.v
+++ verilog/rtl/testchip/control_logic.v
@@ -1,4 +1,5 @@
 `default_nettype  none
+`include "sky130_sram_1kbyte_1rw1r_32x256_8.v"
 
 module control_logic (
     input clk_in,
@@ -8,7 +9,10 @@ module control_logic (
     output [31:0] read_to_pico    
 );
 
+parameter NUM_SRAM = 2 ;
+
 //Connections for R/W Port
+reg [$clog2(NUM_SRAM) - 1:0] chip_select;
 reg csb0_reg;
 reg web0_reg;
 reg [3:0] wmask_reg;
@@ -20,7 +24,8 @@ reg csb1_reg;
 reg [7:0] addr1_reg;
 
 //Capture data from pico into registers
-always @ (???) begin
+always @ (packet) begin
+    chip_select <= packet[]
     csb0_reg <= packet[]
     web0_reg <= packet[]
     wmask_reg <= packet[]
@@ -29,6 +34,69 @@ always @ (???) begin
     
     csb1_reg <= packet[]
     addr1_reg <= packet[]
+end
+
+//Declare wires to connect to SRAM ports
+//SRAM0
+wire sram0_csb0;
+wire sram0_web0;
+wire [3:0] sram0_wmask;
+wire [7:0] sram0_addr0;
+wire [31:0] sram0_din;
+wire [31:0] sram0_dout0;
+
+wire sram0_csb1;
+wire [7:0] sram0_addr1;
+wire [31:0] sram0_dout1;
+
+//SRAM1
+wire sram1_csb0;
+wire sram1_web0;
+wire [3:0] sram1_wmask;
+wire [7:0] sram1_addr0;
+wire [31:0] sram1_din;
+wire [31:0] sram1_dout0;
+
+wire sram1_csb1;
+wire [7:0] sram1_addr1;
+wire [31:0] sram1_dout1;
+
+//Create SRAM Blocks
+
+sky130_sram_1kbyte_1rw1r_32x256_8 SRAM0 (
+        // MGMT R/W port
+        .clk0(clk_in), 
+        .csb0(sram0_csb0),   
+        .web0(sram0_web0),  
+        .wmask0(sram0_wmask),
+        .addr0(sram0_addr0),
+        .din0(sram0_din),
+        .dout0(sram0_dout0),
+        // MGMT RO port
+        .clk1(clk_in),
+        .csb1(sram0_csb1), 
+        .addr1(sram0_addr1),
+        .dout1(sram0_dout1)
+    );
+
+sky130_sram_1kbyte_1rw1r_32x256_8 SRAM1 (
+        // MGMT R/W port
+        .clk0(clk_in), 
+        .csb0(sram1_csb0),   
+        .web0(sram1_web0),  
+        .wmask0(sram1_wmask),
+        .addr0(sram1_addr0),
+        .din0(sram1_din),
+        .dout0(sram1_dout0),
+        // MGMT RO port
+        .clk1(clk_in),
+        .csb1(sram1_csb1), 
+        .addr1(sram1_addr1),
+        .dout1(sram1_dout1)
+    );
+
+always @ (chip_select) begin
+    //Demux values from flops into SRAM
 end
 
 endmodule
