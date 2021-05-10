54fff2afdecfe1df8c2c5f6fa2290dfe1931bb8b
AmoghLonkar <alonkar@ucsc.edu>
2021-05-09 17:31:31 -0700

    Removed compilation errors, need to test

--- verilog/rtl/testchip/control_logic.v
+++ verilog/rtl/testchip/control_logic.v
@@ -7,48 +7,30 @@ module SRAM_IN (
     
     //SRAM0
     //RW Port
-    output mgmt_ena0, 
-    output mgmt_wen0, 
-    output [3:0] mgmt_wen_mask0,  
-    output [7:0] mgmt_addr0,
-    output [31:0] mgmt_wdata0,
+    output reg mgmt_ena0, 
+    output reg mgmt_wen0, 
+    output reg [3:0] mgmt_wen_mask0,  
+    output reg [7:0] mgmt_addr0,
+    output reg [31:0] mgmt_wdata0,
 
     //RO Port
-    output mgmt_ena_ro0,
-    output [7:0] mgmt_addr_ro0,
+    output reg mgmt_ena_ro0,
+    output reg [7:0] mgmt_addr_ro0,
 
     //SRAM1
-    output mgmt_ena1, 
-    output mgmt_wen1, 
-    output [3:0] mgmt_wen_mask1,  
-    output [7:0] mgmt_addr1,
-    output [31:0] mgmt_wdata1,
+    output reg mgmt_ena1, 
+    output reg mgmt_wen1, 
+    output reg [3:0] mgmt_wen_mask1,  
+    output reg [7:0] mgmt_addr1,
+    output reg [31:0] mgmt_wdata1,
 
     //RO Port
-    output mgmt_ena_ro1,
-    output [7:0] mgmt_addr_ro1
+    output reg mgmt_ena_ro1,
+    output reg [7:0] mgmt_addr_ro1
 );
 
 reg [54:0] in_packet;
 reg chip_select;
-
-reg sram0_csb0; 
-reg sram0_web0; 
-reg [3:0] sram0_wmask;  
-reg [7:0] sram0_addr0;
-reg [31:0] sram0_wdata;
-
-reg sram0_csb1;
-reg [7:0] sram0_addr1;
-
-reg sram1_csb0; 
-reg sram1_web0; 
-reg [3:0] sram1_wmask;  
-reg [7:0] sram1_addr0;
-reg [31:0] sram1_wdata;
-
-reg sram1_csb1;
-reg [7:0] sram1_addr1;
 
 always @(packet) begin
     in_packet <= packet[54:0];
@@ -59,65 +41,44 @@ end
 //0's to other SRAM pins
 always @(in_packet, chip_select) begin
     case(chip_select)
-        1'b0 : begin
-            sram0_csb0 = in_packet[54];
-            sram0_web0 = in_packet[53];
-            sram0_wmask = in_packet[52:49];
-            sram0_addr0 = in_packet[48:41];
-            sram0_wdata = in_packet[40:9];
-            sram0_csb1 = in_packet[8];
-            sram0_addr1 = in_packet[7:0];
+    1'b0 : begin
+            mgmt_ena0 <= in_packet[54];
+            mgmt_wen0 <= in_packet[53];
+            mgmt_wen_mask0 <= in_packet[52:49];
+            mgmt_addr0 <= in_packet[48:41];
+            mgmt_wdata0 = in_packet[40:9];
+            mgmt_ena_ro0 = in_packet[8];
+            mgmt_addr_ro0 = in_packet[7:0];
 
-            sram1_csb0 = 0;
-            sram1_web0 = 0;
-            sram1_wmask = 0;
-            sram1_addr0 = 0;
-            sram1_wdata = 0;
-            sram1_csb1 = 0;
-            sram1_addr1 = 0;
+            mgmt_ena1 <= 1'b0;
+            mgmt_wen1 <= 1'b0;
+            mgmt_wen_mask1 <= 4'b0000;
+            mgmt_addr1 <= 8'd0;
+            mgmt_wdata1 = 32'd0;
+            mgmt_ena_ro1 = 1'b0;
+            mgmt_addr_ro1 = 8'd0;
         end 
 
         1'b1 : begin
-            sram1_csb0 = in_packet[54];
-            sram1_web0 = in_packet[53];
-            sram1_wmask = in_packet[52:49];
-            sram1_addr0 = in_packet[48:41];
-            sram1_wdata = in_packet[40:9];
-            sram1_csb1 = in_packet[8];
-            sram1_addr1 = in_packet[7:0];
+            mgmt_ena1 <= in_packet[54];
+            mgmt_wen1 <= in_packet[53];
+            mgmt_wen_mask1 <= in_packet[52:49];
+            mgmt_addr1 <= in_packet[48:41];
+            mgmt_wdata1 = in_packet[40:9];
+            mgmt_ena_ro1 = in_packet[8];
+            mgmt_addr_ro1 = in_packet[7:0];
 
-            sram0_csb0 = 0;
-            sram0_web0 = 0;
-            sram0_wmask = 0;
-            sram0_addr0 = 0;
-            sram0_wdata = 0;
-            sram0_csb1 = 0;
-            sram0_addr1 = 0;
+            mgmt_ena0 <= 1'b0;
+            mgmt_wen0 <= 1'b0;
+            mgmt_wen_mask0 <= 4'b0000;
+            mgmt_addr0 <= 8'd0;
+            mgmt_wdata0 = 32'd0;
+            mgmt_ena_ro0 = 1'b0;
+            mgmt_addr_ro0 = 8'd0;
         end
 
     endcase
-end
 
-always @(*) begin
-    //Connect output to SRAM0
-    mgmt_ena0 = sram0_csb0;
-    mgmt_wen0 = sram0_web0;
-    mgmt_wen_mask0 = sram0_wmask;
-    mgmt_addr0 = sram0_addr0;
-    mgmt_wdata0 = sram0_wdata;
-
-    mgmt_ena_ro0 = sram0_csb1;
-    mgmt_addr_ro0 = sram0_addr1;
-
-    //Connect output to SRAM1
-    mgmt_ena1 = sram1_csb0; 
-    mgmt_wen1 = sram1_web0;
-    mgmt_wen_mask1 = sram1_wmask;
-    mgmt_addr1 = sram1_addr0;
-    mgmt_wdata1 = sram1_wdata;
-
-    mgmt_ena_ro1 = sram1_csb1;
-    mgmt_addr_ro1 = sram1_addr1;
 end
 endmodule
 
@@ -125,16 +86,16 @@ module SRAM_OUT(
     input chip_select,
     input [31:0] sram0_data,
     input [31:0] sram1_data,
-    output [31:0] sram_contents
+    output reg [31:0] sram_contents
 );
 
 //Mux read values from different SRAMS
 //and send to picorv
 always @(sram0_data, sram1_data) begin
     if(chip_select == 0)
-        sram_contents = sram0_data;
+        sram_contents <= sram0_data;
     else if(chip_select == 1)
-        sram_contents = sram1_data;
+        sram_contents <= sram1_data;
 end
 endmodule
 `default_nettype wire 
