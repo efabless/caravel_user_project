33084576f5643e020aeff9cea3d47e3580df4ae4
AmoghLonkar <alonkar@ucsc.edu>
2021-05-17 15:25:42 -0700

    Added clock net and pdn script from professor's repo

--- openlane/openram_testchip/config.tcl
+++ openlane/openram_testchip/config.tcl
@@ -12,6 +12,7 @@ set ::env(VERILOG_FILES) "\
 	$script_dir/../../verilog/rtl/testchip/sky130_sram_1kbyte_1rw1r_32x256_8.v"
 
 set ::env(CLOCK_PORT) "clk_in"
+set ::env(CLOCK_NET) "clk_in"
 set ::env(CLOCK_PERIOD) "10"
 
 # Fill this
@@ -20,9 +21,15 @@ set ::env(FP_PDN_CORE_RING) 0
 set ::env(GLB_RT_MAXLAYER) 5
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 900 900"
+set ::env(DIE_AREA) "0 0 1200 1200"
 
-set ::env(PL_BASIC_PLACEMENT) 0
+set ::env(PL_OPENPHYSYN_OPTIMIZATIONS) 0
+set ::env(PL_RANDOM_GLB_PLACEMENT) 1
+set ::env(PL_BASIC_PLACEMENT) 1
+set ::env(PL_TARGET_DENSITY) 0.99
+
+# Power config
+set ::env(PDN_CFG) $script_dir/pdn.tcl
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
