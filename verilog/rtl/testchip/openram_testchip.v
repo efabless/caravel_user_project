9b0739722a404e8ef6919bb16de77c245ee40fd8
AmoghLonkar <alonkar@ucsc.edu>
2021-05-17 23:52:31 -0700

    Added synthesizable verilog using Chisel

--- verilog/rtl/testchip/openram_testchip.v
+++ verilog/rtl/testchip/openram_testchip.v
@@ -1,16 +1,6 @@
-`default_nettype  none
-`include "control_logic.v"
-`include "sky130_sram_1kbyte_1rw1r_32x256_8.v"
-
 module openram_testchip(
-    input clk_in,
-    input rst,
-    input [55:0] analyzer_packet,
-    input [55:0] gpio_packet,
-    input in_select,  
-    output reg [31:0] sram_data
-   
-    `ifdef USE_POWER_PINS
+
+  `ifdef USE_POWER_PINS
       inout vdda1,        // User area 1 3.3V supply
       inout vdda2,        // User area 2 3.3V supply
       inout vssa1,        // User area 1 analog ground
@@ -20,138 +10,85 @@ module openram_testchip(
       inout vssd1,        // User area 1 digital ground
       inout vssd2,        // User area 2 digital ground
   `endif
+  
+  input         clock,
+  input         reset,
+  input  [55:0] io_logical_analyzer_packet,
+  input  [55:0] io_gpio_packet,
+  input         io_in_select,
+  input  [31:0] io_sram0_rw_in,
+  input  [31:0] io_sram0_r0_in,
+  input  [31:0] io_sram1_rw_in,
+  input  [31:0] io_sram1_ro_in,
+  output [54:0] io_sram0_connections,
+  output [54:0] io_sram1_connections,
+  output [31:0] io_sram_data
 );
-
-reg [54:0] packet;
-reg chip_select;
-
-//SRAM0
-//RW Port
-wire mgmt_ena0; 
-wire mgmt_wen0; 
-wire [3:0] mgmt_wen_mask0;  
-wire [7:0] mgmt_addr0;
-wire [31:0] mgmt_wdata0;
-wire [31:0] mgmt_rdata0;
-//RO Port
-wire mgmt_ena_ro0;
-wire [7:0] mgmt_addr_ro0;
-wire [31:0] mgmt_rdata_ro0;
-
-//SRAM1
-wire mgmt_ena1; 
-wire mgmt_wen1; 
-wire [3:0] mgmt_wen_mask1;  
-wire [7:0] mgmt_addr1;
-wire [31:0] mgmt_wdata1;
-wire [31:0] mgmt_rdata1;
-//RO Port
-wire mgmt_ena_ro1;
-wire [7:0] mgmt_addr_ro1;
-wire [31:0] mgmt_rdata_ro1;
-
-//Data from SRAMs
-wire [31:0] sram0_data;
-wire [31:0] sram1_data;
-wire [31:0] read_data;
-
-//Read in input packet from analyzer or GPIO pins
-always @(posedge clk_in or rst) begin
-    if(rst) begin
-        chip_select <= 0;
-        packet <= 0;
+`ifdef RANDOMIZE_REG_INIT
+  reg [63:0] _RAND_0;
+`endif // RANDOMIZE_REG_INIT
+  reg [55:0] input_; // @[openram_testchip.scala 22:20]
+  wire  chip_select = input_[55]; // @[openram_testchip.scala 29:28]
+  wire  csb0 = input_[54]; // @[openram_testchip.scala 30:21]
+  wire  web = input_[53]; // @[openram_testchip.scala 31:20]
+  wire  _T = ~chip_select; // @[Conditional.scala 37:30]
+  wire [54:0] _GEN_0 = chip_select ? input_[54:0] : 55'h7fffffffffffff; // @[Conditional.scala 39:67 openram_testchip.scala 42:34 openram_testchip.scala 34:26]
+  wire [31:0] _io_sram_data_T = csb0 ? io_sram1_ro_in : io_sram1_rw_in; // @[openram_testchip.scala 49:45]
+  wire [31:0] _io_sram_data_T_1 = csb0 ? io_sram0_r0_in : io_sram0_rw_in; // @[openram_testchip.scala 49:88]
+  wire [31:0] _io_sram_data_T_2 = chip_select ? _io_sram_data_T : _io_sram_data_T_1; // @[openram_testchip.scala 49:28]
+  assign io_sram0_connections = _T ? input_[54:0] : 55'h7fffffffffffff; // @[Conditional.scala 40:58 openram_testchip.scala 38:34 openram_testchip.scala 33:26]
+  assign io_sram1_connections = _T ? 55'h7fffffffffffff : _GEN_0; // @[Conditional.scala 40:58 openram_testchip.scala 34:26]
+  assign io_sram_data = web ? _io_sram_data_T_2 : 32'h0; // @[openram_testchip.scala 48:14 openram_testchip.scala 49:22 openram_testchip.scala 46:18]
+  always @(posedge clock) begin
+    if (io_in_select) begin // @[openram_testchip.scala 27:17]
+      input_ <= io_gpio_packet;
+    end else begin
+      input_ <= io_logical_analyzer_packet;
     end
-    
-    else begin
-        if(in_select == 0) begin
-            chip_select <= analyzer_packet[55]; 
-            packet <= analyzer_packet[54:0];
-        end
-        else if(in_select == 1) begin
-            chip_select <= gpio_packet[55]; 
-            packet <= gpio_packet[54:0];
-        end
-    end
-end
-
-//Instantitate port connections
-SRAM_IN in_control(.clk_in(clk_in),
-                   .rst(rst),
-                   .chip_select(chip_select),
-                   .packet(packet),
-                   .mgmt_ena0(mgmt_ena0),
-                   .mgmt_wen0(mgmt_wen0),
-                   .mgmt_wen_mask0(mgmt_wen_mask0),
-                   .mgmt_addr0(mgmt_addr0),
-                   .mgmt_wdata0(mgmt_wdata0),
-                   .mgmt_ena_ro0(mgmt_ena_ro0),
-                   .mgmt_addr_ro0(mgmt_addr_ro0),
-                   .mgmt_ena1(mgmt_ena1),
-                   .mgmt_wen1(mgmt_wen1),
-                   .mgmt_wen_mask1(mgmt_wen_mask1),
-                   .mgmt_addr1(mgmt_addr1),
-                   .mgmt_wdata1(mgmt_wdata1),
-                   .mgmt_ena_ro1(mgmt_ena_ro1),
-                   .mgmt_addr_ro1(mgmt_addr_ro1)
-);
-
-sky130_sram_1kbyte_1rw1r_32x256_8 SRAM_0 (
-// MGMT R/W port
-    .clk0(clk_in), 
-    .csb0(mgmt_ena0),   
-    .web0(mgmt_wen0),  
-    .wmask0(mgmt_wen_mask0),
-    .addr0(mgmt_addr0),
-    .din0(mgmt_wdata0),
-    .dout0(mgmt_rdata0),
-    // MGMT RO port
-    .clk1(clk_in),
-    .csb1(mgmt_ena_ro0), 
-    .addr1(mgmt_addr_ro0),
-    .dout1(mgmt_rdata_ro0)
-); 
-
-sky130_sram_1kbyte_1rw1r_32x256_8 SRAM_1 (
-    // MGMT R/W port
-     .clk0(clk_in), 
-    .csb0(mgmt_ena1),   
-    .web0(mgmt_wen1),  
-    .wmask0(mgmt_wen_mask1),
-    .addr0(mgmt_addr1),
-    .din0(mgmt_wdata1),
-    .dout0(mgmt_rdata1),
-    // MGMT RO port
-    .clk1(clk_in),
-    .csb1(mgmt_ena_ro1), 
-    .addr1(mgmt_addr_ro1),
-    .dout1(mgmt_rdata_ro1)
-);  
-
-SRAM_DATA sram0_out(.csb0(packet[54]),
-          .rst(rst),
-          .csb1(packet[8]),
-          .dout0(mgmt_rdata0),
-          .dout1(mgmt_rdata_ro0),
-          .sram_data(sram0_data)
-);
-
-SRAM_DATA sram1_out(.csb0(packet[54]),
-          .rst(rst),
-          .csb1(packet[8]),
-          .dout0(mgmt_rdata1),
-          .dout1(mgmt_rdata_ro1),
-          .sram_data(sram1_data)
-);
-
-SRAM_OUT out_control(.chip_select(chip_select),
-                     .rst(rst),
-                     .sram0_data(sram0_data),
-                     .sram1_data(sram1_data),
-                     .sram_contents(read_data)
-);
-
-always @(posedge clk_in) begin
-    sram_data <= read_data;
-end
+  end
+// Register and memory initialization
+`ifdef RANDOMIZE_GARBAGE_ASSIGN
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_INVALID_ASSIGN
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_REG_INIT
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_MEM_INIT
+`define RANDOMIZE
+`endif
+`ifndef RANDOM
+`define RANDOM $random
+`endif
+`ifdef RANDOMIZE_MEM_INIT
+  integer initvar;
+`endif
+`ifndef SYNTHESIS
+`ifdef FIRRTL_BEFORE_INITIAL
+`FIRRTL_BEFORE_INITIAL
+`endif
+initial begin
+  `ifdef RANDOMIZE
+    `ifdef INIT_RANDOM
+      `INIT_RANDOM
+    `endif
+    `ifndef VERILATOR
+      `ifdef RANDOMIZE_DELAY
+        #`RANDOMIZE_DELAY begin end
+      `else
+        #0.002 begin end
+      `endif
+    `endif
+`ifdef RANDOMIZE_REG_INIT
+  _RAND_0 = {2{`RANDOM}};
+  input_ = _RAND_0[55:0];
+`endif // RANDOMIZE_REG_INIT
+  `endif // RANDOMIZE
+end // initial
+`ifdef FIRRTL_AFTER_INITIAL
+`FIRRTL_AFTER_INITIAL
+`endif
+`endif // SYNTHESIS
 endmodule
-`default_nettype wire
