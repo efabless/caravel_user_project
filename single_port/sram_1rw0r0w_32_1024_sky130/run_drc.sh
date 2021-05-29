9efc60905b6a481f2afe758d6ada52329c3905c3
Jesse Cirimelli-Low <jcirimel@ucsc.edu>
2021-05-28 19:37:28 -0700

    add singleport memories

---
+++ single_port/sram_1rw0r0w_32_1024_sky130/run_drc.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+export OPENRAM_TECH="/home/odette/openram/technology"
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__openram_dp_cell.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__openram_dp_cell_dummy.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__openram_dp_cell_replica.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_cell_opt1a.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__openram_sp_cell_opt1a_dummy.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_cell_opt1_ce.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_cell_opt1.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__openram_sp_cell_opt1_replica.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__openram_sp_cell_opt1a_replica.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_colend.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_colend_cent.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_colend_p_cent.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_colenda.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_colenda_cent.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_colenda_p_cent.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_rowend.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_rowenda.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__openram_sp_rowend_replica.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__openram_sp_rowenda_replica.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_corner.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_cornera.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_cornerb.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_wlstrapa.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_wlstrap_ce.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_wlstrap.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_wlstrap_p_ce.mag .
+cp /home/odette/openram/technology/sky130/maglef_lib/sky130_fd_bd_sram__sram_sp_wlstrap_p.mag .
+echo "$(date): Starting DRC using Magic /usr/local/bin/magic"
+
+/usr/local/bin/magic -dnull -noconsole << EOF
+load sram_1rw0r0w_32_1024_sky130 -dereference
+puts "Finished loading cell sram_1rw0r0w_32_1024_sky130"
+cellname delete \(UNNAMED\)
+select top cell
+expand
+puts "Finished expanding"
+drc euclidean on
+drc check
+puts "Finished drc check"
+drc catchup
+puts "Finished drc catchup"
+drc count total
+quit -noprompt
+EOF
+magic_retcode=$?
+echo "$(date): Finished ($magic_retcode) DRC using Magic /usr/local/bin/magic"
+exit $magic_retcode
