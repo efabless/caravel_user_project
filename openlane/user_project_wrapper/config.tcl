d7ce953d046e6b4e3dcea1e8b102b1e7e0462098
AmoghLonkar <alonkar@ucsc.edu>
2021-05-31 09:29:20 -0700

    Included single port files and fixed gds error

--- openlane/user_project_wrapper/config.tcl
+++ openlane/user_project_wrapper/config.tcl
@@ -43,21 +43,32 @@ set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro.cfg
 ### Black-box verilog and views
 set ::env(VERILOG_FILES_BLACKBOX) "\
 	$script_dir/../../caravel/verilog/rtl/defines.v \
-	$script_dir/../../verilog/rtl/sky130_sram_1kbyte_1rw1r_32x256_8.v "
+	$script_dir/../../verilog/rtl/sky130_sram_1kbyte_1rw1r_32x256_8.v \
+	$script_dir/../../verilog/rtl/sram_1rw0r0w_32_1024_sky130.v \
+	$script_dir/../../verilog/rtl/sram_1rw0r0w_32_256_sky130.v \
+	$script_dir/../../verilog/rtl/sram_1rw0r0w_32_512_sky130.v \
+	$script_dir/../../verilog/rtl/sram_1rw0r0w_64_512_sky130.v "
 
 set ::env(EXTRA_LEFS) "\
 	$script_dir/../../lef/user_project_wrapper.lef \
-	$script_dir/../../lef/sky130_sram_1kbyte_1rw1r_32x256_8.lef"
+	$script_dir/../../lef/sky130_sram_1kbyte_1rw1r_32x256_8.lef \
+	$script_dir/../../lef/sram_1rw0r0w_32_1024_sky130.lef \
+	$script_dir/../../lef/sram_1rw0r0w_32_256_sky130.lef \
+	$script_dir/../../lef/sram_1rw0r0w_32_512_sky130.lef \
+	$script_dir/../../lef/sram_1rw0r0w_64_512_sky130.lef "
 
 set ::env(EXTRA_GDS_FILES) "\
 	$script_dir/../../gds/user_project_wrapper.gds \
-	$script_dir/../../lef/sky130_sram_1kbyte_1rw1r_32x256_8.gds"
+	$script_dir/../../gds/sky130_sram_1kbyte_1rw1r_32x256_8.gds \
+	$script_dir/../../gds/sram_1rw0r0w_32_1024_sky130.gds \
+	$script_dir/../../gds/sram_1rw0r0w_32_256_sky130.gds \
+	$script_dir/../../gds/sram_1rw0r0w_32_512_sky130.gds \
+	$script_dir/../../gds/sram_1rw0r0w_64_512_sky130.gds "
 
 set ::env(GLB_RT_MAXLAYER) 5
 
 set ::env(FP_PDN_CHECK_NODES) 0
 # Power config
-#set ::env(PDN_CFG) $script_dir/pdn.tcl
 
 #set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
@@ -66,8 +77,8 @@ set ::env(PL_OPENPHYSYN_OPTIMIZATIONS) 0
 
 set ::env(PL_RANDOM_GLB_PLACEMENT) 1
 
-#set ::env(PL_BASIC_PLACEMENT) 1
-#set ::env(PL_TARGET_DENSITY) 0.99
+set ::env(GLB_RT_ADJUSTMENT) 0.20
+set ::env(PL_TARGET_DENSITY) 0.40
 
 #set ::env(MAGIC_DRC_USE_GDS) 0
 
