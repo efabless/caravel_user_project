d6f65a9fd9eb3141312d62a8a407ecd5c804ac6d
AmoghLonkar <alonkar@ucsc.edu>
2021-06-10 12:07:04 -0700

    sram_clk toggles as expected

--- verilog/rtl/openram_testchip.v
+++ verilog/rtl/openram_testchip.v
@@ -39,7 +39,7 @@ reg clk;
 reg sram_clk;
 reg web;
 reg csb0;
-reg [64:0] read_data;
+reg toggle_clk;
 
 always @(*) begin
     clk = in_select ? gpio_clock : wb_clock;
@@ -55,74 +55,72 @@ always @ (posedge clk) begin
     else begin
         input_connection <= la_packet[82:0];
         chip_select <= la_packet[85:83];
-        sram_clk <= 1;
-        if(sram_clk) begin
-            sram_clk <= 0;
-        end
     end
 end 
 
-always @(*) begin
-    case(chip_select)
-    3'd0: begin
-        csb0 <= input_connection[54];
-        web <= input_connection[53]; 
-    end
-    3'd1: begin
-        csb0 <= input_connection[54];
-        web <= input_connection[53];
-    end 
-    3'd2: web <= input_connection[46];
-    3'd3: web <= input_connection[44];
-    3'd4: web <= input_connection[45];
-    3'd5: web <= input_connection[81];
-    default : begin
-        csb0 <= 1;
-        web <= 1;
-    end
-    endcase
+always @(input_connection) begin
+    toggle_clk <= 1;
 end
 
-always @ (posedge clk) begin
-    sram0_connections <= (chip_select == 0) ? {sram_clk, input_connection[54:0]} : {56{1'b0}};
-    sram1_connections <= (chip_select == 1) ? {sram_clk, input_connection[54:0]} : {56{1'b0}};
-    sram2_connections <= (chip_select == 2) ? {sram_clk, input_connection[47:0]} : {48{1'b0}};
-    sram3_connections <= (chip_select == 3) ? {sram_clk, input_connection[45:0]} : {46{1'b0}};
-    sram4_connections <= (chip_select == 4) ? {sram_clk, input_connection[46:0]} : {47{1'b0}};
-    sram5_connections <= (chip_select == 5) ? {sram_clk, input_connection[82:0]} : {83{1'b0}};
-end
-
-/*
-always @ (posedge clk) begin
-    if(sram_clk) begin
-        sram_clk <= 0;
-    end
-end
-*/
-always @ (posedge clk) begin
-    if(web) begin
-        case(chip_select)
-        /*
-        3'd0: read_data <= {32'd0, csb0 ? sram0_ro_in : sram0_rw_in};
-        3'd1: read_data <= {32'd0, csb0 ? sram1_ro_in : sram1_rw_in};
-        3'd2: read_data <= {32'd0, sram2_rw_in};
-        3'd3: read_data <= {32'd0, sram2_rw_in};
-        3'd4: read_data <= {32'd0, sram2_rw_in};
-        3'd5: read_data <= sram2_rw_in;
-        */
-        3'd0: read_data <= csb0 ? sram0_ro_in : sram0_rw_in;
-        3'd1: read_data <= csb0 ? sram1_ro_in : sram1_rw_in;
-        3'd2: read_data <= sram2_rw_in;
-        3'd3: read_data <= sram3_rw_in;
-        3'd4: read_data <= sram4_rw_in;
-        3'd5: read_data <= sram5_rw_in;
-        default: read_data <= 64'd0;
-        endcase
+always @(posedge clk) begin
+    toggle_clk <= toggle_clk;
+    if(toggle_clk) begin
+            toggle_clk <= 0;
     end
 end
 
 always @(posedge clk) begin
-    sram_data <= read_data;
+    sram_clk <= toggle_clk;   
+end
+
+always @(posedge clk) begin
+    if(reset) begin
+        csb0 <= 1;
+        web <= 1;
+    end
+    else begin
+        case(chip_select)
+        3'd0: begin
+            csb0 <= input_connection[54];
+            web <= input_connection[53]; 
+        end
+        3'd1: begin
+            csb0 <= input_connection[54];
+            web <= input_connection[53];
+        end 
+        3'd2: web <= input_connection[46];
+        3'd3: web <= input_connection[44];
+        3'd4: web <= input_connection[45];
+        3'd5: web <= input_connection[81];
+        default : begin
+            csb0 <= 1;
+            web <= 1;
+        end
+        endcase
+    end
+end
+
+always @ (posedge clk) begin
+    sram0_connections <= (chip_select == 0) ? {sram_clk || toggle_clk, input_connection[54:0]} : {56{1'b0}};
+    sram1_connections <= (chip_select == 1) ? {sram_clk || toggle_clk, input_connection[54:0]} : {56{1'b0}};
+    sram2_connections <= (chip_select == 2) ? {sram_clk || toggle_clk, input_connection[47:0]} : {48{1'b0}};
+    sram3_connections <= (chip_select == 3) ? {sram_clk || toggle_clk, input_connection[45:0]} : {46{1'b0}};
+    sram4_connections <= (chip_select == 4) ? {sram_clk || toggle_clk, input_connection[46:0]} : {47{1'b0}};
+    sram5_connections <= (chip_select == 5) ? {sram_clk || toggle_clk, input_connection[82:0]} : {83{1'b0}};
+end
+
+always @ (posedge clk) begin
+    if(web) begin
+        case(chip_select)
+        3'd0: sram_data <= csb0 ? sram0_ro_in : sram0_rw_in;
+        3'd1: sram_data <= csb0 ? sram1_ro_in : sram1_rw_in;
+        3'd2: sram_data <= sram2_rw_in;
+        3'd3: sram_data <= sram3_rw_in;
+        3'd4: sram_data <= sram4_rw_in;
+        3'd5: sram_data <= sram5_rw_in;
+        default: sram_data <= 64'd0;
+        endcase
+    end
 end
 
 endmodule
