34e47792402608e23c37711aa0839df580d24e6e
mrg <mrg@ucsc.edu>
2021-06-13 17:11:58 -0700

    Make macro almost chip height

--- openlane/openram_testchip/config.tcl
+++ openlane/openram_testchip/config.tcl
@@ -14,7 +14,7 @@ set ::env(CLOCK_NET) "clk"
 set ::env(CLOCK_PERIOD) "10"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 1000 2000"
+set ::env(DIE_AREA) "0 0 800 2900"
 set ::env(DESIGN_IS_CORE) 0
 
 set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
@@ -22,6 +22,7 @@ set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
 
 #set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
+#set ::env(SYNTH_NO_FLAT) 1
 set ::env(GLB_RT_ADJUSTMENT) 0.20
 set ::env(RUN_KLAYOUT_DRC) 0
 set ::env(PL_TARGET_DENSITY) 0.40
