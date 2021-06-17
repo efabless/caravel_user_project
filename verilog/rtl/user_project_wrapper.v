9ccd2e3fd21d747e8440b5994af3cccac51a5f5f
AmoghLonkar <alonkar@ucsc.edu>
2021-06-17 10:14:13 -0700

    Added reg declaration for sram_data and removed duplicate sram_dout declaration

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -92,39 +92,6 @@ module user_project_wrapper #(
    wire [`MAX_CHIPS-1:0]  csb0;
    wire [`MAX_CHIPS-1:0]  csb1;
 
-
-
-   wire [31:0] 		  sram0_dout0;
-   wire [31:0] 		  sram0_dout1;
-   wire [31:0] 		  sram1_dout0;
-   wire [31:0] 		  sram1_dout1;
-   wire [31:0] 		  sram2_dout0;
-   wire [31:0] 		  sram2_dout1;
-   wire [31:0] 		  sram3_dout0;
-   wire [31:0] 		  sram3_dout1;
-   wire [31:0] 		  sram4_dout0;
-   wire [31:0] 		  sram4_dout1;
-   wire [31:0] 		  sram5_dout0;
-   wire [31:0] 		  sram5_dout1;
-   wire [31:0] 		  sram6_dout0;
-   wire [31:0] 		  sram6_dout1;
-   wire [31:0] 		  sram7_dout0;
-   wire [31:0] 		  sram7_dout1;
-   wire [31:0] 		  sram8_dout0;
-   wire [31:0] 		  sram8_dout1;
-   wire [31:0] 		  sram9_dout0;
-   wire [31:0] 		  sram9_dout1;
-   wire [31:0] 		  sram10_dout0;
-   wire [31:0] 		  sram10_dout1;
-   wire [31:0] 		  sram11_dout0;
-   wire [31:0] 		  sram11_dout1;
-   wire [31:0] 		  sram12_dout0;
-   wire [31:0] 		  sram12_dout1;
-   wire [31:0] 		  sram13_dout0;
-   wire [31:0] 		  sram13_dout1;
-   wire [31:0] 		  sram14_dout0;
-   wire [31:0] 		  sram14_dout1;
-
    wire     in_select = io_in[16];
    wire     resetn = io_in[15];
    wire     gpio_clk = io_in[17];
@@ -453,6 +420,40 @@ sram_1rw0r0w_64_512_sky130 SRAM11
    assign sram11_dout1 = 0;
    assign sram11_dout0 = {temp_sram11_dout0[64:49], temp_sram11_dout0[15:0]};
 
+// Hold dout from SRAM
+// clocked by SRAM clk
+reg [`DATA_SIZE-1:0] sram0_data0;
+reg [`DATA_SIZE-1:0] sram0_data1;
+reg [`DATA_SIZE-1:0] sram1_data0;
+reg [`DATA_SIZE-1:0] sram1_data1;
+reg [`DATA_SIZE-1:0] sram2_data0;
+reg [`DATA_SIZE-1:0] sram2_data1;
+reg [`DATA_SIZE-1:0] sram3_data0;
+reg [`DATA_SIZE-1:0] sram3_data1;
+reg [`DATA_SIZE-1:0] sram4_data0;
+reg [`DATA_SIZE-1:0] sram4_data1;
+//reg [`DATA_SIZE-1:0] sram5_data0;
+//reg [`DATA_SIZE-1:0] sram5_data1;
+//reg [`DATA_SIZE-1:0] sram6_data0;
+//reg [`DATA_SIZE-1:0] sram6_data1;
+//reg [`DATA_SIZE-1:0] sram7_data0;
+//reg [`DATA_SIZE-1:0] sram7_data1;
+reg [`DATA_SIZE-1:0] sram8_data0;
+reg [`DATA_SIZE-1:0] sram8_data1;
+reg [`DATA_SIZE-1:0] sram9_data0;
+reg [`DATA_SIZE-1:0] sram9_data1;
+reg [`DATA_SIZE-1:0] sram10_data0;
+reg [`DATA_SIZE-1:0] sram10_data1;
+reg [`DATA_SIZE-1:0] sram11_data0;
+reg [`DATA_SIZE-1:0] sram11_data1;
+//reg [`DATA_SIZE-1:0] sram12_data0;
+//reg [`DATA_SIZE-1:0] sram12_data1;
+//reg [`DATA_SIZE-1:0] sram13_data0;
+//reg [`DATA_SIZE-1:0] sram13_data1;
+//reg [`DATA_SIZE-1:0] sram14_data0;
+//reg [`DATA_SIZE-1:0] sram14_data1;
+//reg [`DATA_SIZE-1:0] sram15_data0;
+//reg [`DATA_SIZE-1:0] sram15_data1;
 
 always @(posedge sram_clk) begin
    if (!resetn) begin
