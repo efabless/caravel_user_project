8719f46ebbd0401769f4c09a4dfb8bc09919cf82
manarabdelaty <manarabdelatty@aucegypt.edu>
2021-09-17 18:22:24 +0200

    [DATA] Update macros (hardened with latest openlane)

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -39,14 +39,8 @@ module user_proj_example #(
     parameter BITS = 32
 )(
 `ifdef USE_POWER_PINS
-    inout vdda1,	// User area 1 3.3V supply
-    inout vdda2,	// User area 2 3.3V supply
-    inout vssa1,	// User area 1 analog ground
-    inout vssa2,	// User area 2 analog ground
     inout vccd1,	// User area 1 1.8V supply
-    inout vccd2,	// User area 2 1.8v supply
     inout vssd1,	// User area 1 digital ground
-    inout vssd2,	// User area 2 digital ground
 `endif
 
     // Wishbone Slave ports (WB MI A)
