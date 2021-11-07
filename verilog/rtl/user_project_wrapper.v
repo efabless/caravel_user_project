2e21655ab78215bf4bbc9611ce5ae4b9e4e656c8
mrg <mrg@ucsc.edu>
2021-11-07 07:34:40 -0800

    Add missing disconnected wire declaration

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -345,6 +345,9 @@ sky130_sram_8kbyte_1rw1r_32x2048_8 SRAM4
 
 
 // Single port memories
+
+wire disconn8, disconn9, disconn10, disconn11, disconn12;
+
 sky130_sram_1kbyte_1rw_32x256_8 SRAM8
     (
       `ifdef USE_POWER_PINS
