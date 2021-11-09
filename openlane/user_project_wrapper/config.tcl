0a4a2e22f83289b38806849657c74afb7c03c653
hadirkhan10 <hadirkhan10@gmail.com>
2021-11-08 21:20:46 -0800

    fixed syntax error related to a missing enclosing bracket

--- openlane/user_project_wrapper/config.tcl
+++ openlane/user_project_wrapper/config.tcl
@@ -33,7 +33,7 @@ set ::env(VERILOG_FILES) "\
 ## Clock configurations
 set ::env(CLOCK_PORT) {io_in\[17\]}
 set ::env(CLOCK_NET) "CONTROL_LOGIC.clk"
-set ::env(RESET_PORT) {io_in\[15\] wb_rst_i
+set ::env(RESET_PORT) {io_in\[15\] wb_rst_i}
 
 set ::env(CLOCK_PERIOD) "30"
 set ::env(BASE_SDC_FILE) "$script_dir/base.sdc"
