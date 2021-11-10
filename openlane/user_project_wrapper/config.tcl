4dfef12259a8f1d4895adb5e114b4d9e3c18a910
mrg <mrg@ucsc.edu>
2021-11-09 16:08:14 -0800

    Config needs escaped brackets

--- openlane/user_project_wrapper/config.tcl
+++ openlane/user_project_wrapper/config.tcl
@@ -37,9 +37,9 @@ set ::env(VERILOG_FILES) "\
 	$script_dir/../../verilog/rtl/testchip/openram_testchip.v"
 
 ## Clock configurations
-set ::env(CLOCK_PORT) {io_in[17]}
+set ::env(CLOCK_PORT) {io_in\[17\]}
 set ::env(CLOCK_NET) "CONTROL_LOGIC.clk"
-set ::env(RESET_PORT) {io_in[15] wb_rst_i}
+set ::env(RESET_PORT) {io_in\[15\] wb_rst_i}
 
 set ::env(CLOCK_PERIOD) "30"
 set ::env(BASE_SDC_FILE) "$script_dir/base.sdc"
