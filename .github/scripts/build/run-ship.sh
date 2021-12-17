1ddb471475ade379c7263f9aa66b94b05b0a9735
Jeff DiCorpo <42048757+jeffdi@users.noreply.github.com>
2021-12-17 12:56:10 -0800

    Update run-ship.sh

--- .github/scripts/build/run-ship.sh
+++ .github/scripts/build/run-ship.sh
@@ -25,6 +25,8 @@ cd $UPRJ_ROOT
 # Install full version of caravel
 git clone https://github.com/efabless/caravel --depth 1 $CARAVEL_ROOT
 
+make install_mcw
+
 docker run -v $UPRJ_ROOT:$UPRJ_ROOT -v $PDK_ROOT:$PDK_ROOT -v $CARAVEL_ROOT:$CARAVEL_ROOT -e UPRJ_ROOT=$UPRJ_ROOT -e PDK_ROOT=$PDK_ROOT -e CARAVEL_ROOT=$CARAVEL_ROOT -u $(id -u $USER):$(id -g $USER) $IMAGE_NAME bash -c "cd $CARAVEL_ROOT; make uncompress; cd $UPRJ_ROOT; make ship;"
 
 SHIP_FILE=$UPRJ_ROOT/gds/caravel.gds
