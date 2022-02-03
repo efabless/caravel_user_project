c6a9cfdcb1e386cf1f8d7af89100943e050b4153
a-omla <47463234+a-omla@users.noreply.github.com>
2022-02-03 21:48:33 +0200

    EFIC-233 - Update run-precheck-drc.sh

--- .github/scripts/precheck/run-precheck-drc.sh
+++ .github/scripts/precheck/run-precheck-drc.sh
@@ -23,7 +23,7 @@ cd mpw_precheck
 docker run -v $PRECHECK_ROOT:$PRECHECK_ROOT -v $TARGET_PATH:$TARGET_PATH -v $PDK_ROOT:$PDK_ROOT \
 -u $(id -u $USER):$(id -g $USER) efabless/mpw_precheck:latest bash -c "cd $PRECHECK_ROOT; python3 mpw_precheck.py \
     --input_directory $INPUT_DIRECTORY --pdk_root $PDK_ROOT --output_directory $OUTPUT_DIRECTORY \
-    magic_drc klayout_offgrid klayout_feol klayout_zeroarea klayout_pin_label_purposes_overlapping_drawing
+    magic_drc klayout_beol klayout_feol klayout_met_min_ca_density klayout_offgrid klayout_pin_label_purposes_overlapping_drawing klayout_zeroarea
 
 output=$OUTPUT_DIRECTORY/logs/precheck.log
 
