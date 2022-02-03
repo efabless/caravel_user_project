2957d2b345c4f4190a4c90772733942c4bcfc4cf
Ahmed El Omla <ahmed.el-omla@efabless.com>
2022-02-03 22:44:44 +0200

    EFIC-233 - update ci

--- .github/scripts/precheck/install-magic.sh
+++ .github/scripts/precheck/install-magic.sh
@@ -17,12 +17,11 @@
 # By default build pdk since we don't need the other script for the main purpose
 export TARGET_PATH=$(pwd)
 
-sudo apt-get install tcl tk tcl-dev tk-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev
+sudo apt-get install -y tcl tk tcl-dev tk-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev csh libncurses5-dev libncursesw5-dev libx11-dev
 
 export MAGIC_ROOT=./magic
 git clone --depth=1 --branch 8.3.263 https://github.com/RTimothyEdwards/magic.git ${MAGIC_ROOT}
 cd $MAGIC_ROOT
 ./configure --disable-locking --prefix=/build CFLAGS='-g -O0 -m64 -fPIC' && make -j4 && make install
-
 
 exit 0
