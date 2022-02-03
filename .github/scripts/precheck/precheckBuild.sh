b3fc71923be5aa373ff01edf56a1d78598ef8a97
a-omla <47463234+a-omla@users.noreply.github.com>
2022-02-03 21:40:00 +0200

    EFIC-233 - update precheckBuild.sh

--- .github/scripts/precheck/precheckBuild.sh
+++ .github/scripts/precheck/precheckBuild.sh
@@ -18,6 +18,7 @@
 export SKIP_PDK_BUILD=${1:-0}
 export TARGET_PATH=$(pwd)
 
+git clone --depth=1 https://github.com/efabless/mpw_precheck.git
 git clone --depth=1 https://github.com/efabless/caravel-lite.git
 
 docker pull efabless/mpw_precheck:latest
