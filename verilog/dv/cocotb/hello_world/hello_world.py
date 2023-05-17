4b071462a985545ab3852360adac069e248b41b0
NouranAbdelaziz <nouran.abdelaziz@aucegpt.edu>
2023-05-17 12:21:28 +0300

    added gpio test

--- verilog/dv/cocotb/hello_world/hello_world.py
+++ verilog/dv/cocotb/hello_world/hello_world.py
@@ -2,7 +2,7 @@ from cocotb_includes import *
 
 
 @cocotb.test()
-@repot_test
+@report_test
 async def hello_world(dut):
     caravelEnv = await test_configure(dut,timeout_cycles=9373)
     cocotb.log.info("Hello World")
