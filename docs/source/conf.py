3a369d5c9b277c9b274ebb5c86722da7e7e0af42
Marwan Abbas <67271180+marwaneltoukhy@users.noreply.github.com>
2023-11-12 14:53:01 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,25 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 # SPDX-License-Identifier: Apache-2.0
 
 # Configuration file for the Sphinx documentation builder.
-# Yes, it needs to be in this directory. Don't try to move it.
-# Yes, it needs to be called conf.py
-
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
-import os
-import sys
 
-sys.path.insert(0, os.path.abspath("../_ext"))
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
 
 # -- Project information -----------------------------------------------------
-project = "caravel_user_project"
-copyright = "2020-2023 Efabless Corporation and contributors"
-author = "Efabless Corporation"
-repo = "https://github.com/efabless/caravel_user_project"
-branch = "main"
+
+project = 'CIIC Harness'
+copyright = '2020, efabless'
+author = 'efabless'
 
 
 # -- General configuration ---------------------------------------------------
@@ -43,14 +49,8 @@ extensions = [
   'sphinx.ext.todo'
 ]
 
-source_suffix = {
-    ".rst": "restructuredtext",
-    ".md": "markdown",
-}
-
-
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ["_templates"]
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -64,51 +64,26 @@ exclude_patterns = [
 
 
 # -- Options for HTML output -------------------------------------------------
-
+"""
+html_theme_options = {
+    'header_links' : [
+        ("Home", 'index', False, 'home'),
+        ("GitHub", "https://github.com/efabless/caravel", True, 'code'),
+    ],
+    'hide_symbiflow_links': True,
+    'license_url' : 'https://www.apache.org/licenses/LICENSE-2.0',
+}
+"""
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_title = "Caravel User Project Documentation"
-
-html_theme = "furo"
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {
-    "source_repository": repo,
-    "source_branch": branch,
-    "source_directory": "docs/source",
-    "footer_icons": [
-        {
-            "name": "GitHub",
-            "url": repo,
-            "html": """
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                </svg>
-            """,
-            "class": "",
-        },
-    ],
-}
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["../_static"]
+html_static_path = ['_static']
 
-todo_include_todos = True
+todo_include_todos = False
+
 numfig = True
-autosectionlabel_prefix_document = True
-
-suppress_warnings = ["misc.highlighting_failure"]  # supress json highlight warnings
-
-myst_heading_anchors = 3
-
-myst_enable_extensions = [
-    "colon_fence",
-    "attrs_block",
-]
-
-root_doc = "index"
